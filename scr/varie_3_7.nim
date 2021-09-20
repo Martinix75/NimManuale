2d0f8ca47c8bc788185579184ced90ca99c6486b
Martinix75 <fintobersaglio@yahoo.it>
2021-09-20 23:40:02 +0200

    Strutture dati

--- scr/varie_3_7.nim
+++ scr/varie_3_7.nim
@@ -14,8 +14,8 @@ if 55 notin se1:
 echo (fmt"array1= {ar1} sequenza1= {se1} e contiene {len(se1)} elementi")
 
 var set1 = toHashSet(se1)
-echo (fmt"array1= {ar1} sequenza1= {set1} e contiene {len(set1)} elementi")
-echo (fmt"Da elem 1->3 {se1[1..3]}")
+echo (fmt"array1= {ar1} hashset= {set1} e contiene {len(set1)} elementi")
+echo (fmt"Da elem 1->3 di Sequenza se1{se1[1..3]}")
 echo("Lista elementi array:")
 for c in ar1.low..ar1.high:
     stdout.write(ar1[c]," ")
@@ -26,4 +26,4 @@ echo()
 var `inc` = 0
 for e in 0..5:
     inc.inc
-    echo(fmt("Ora incerement vale: {inc}"))
+    echo(fmt("Ora inceremento vale: {inc}"))
