e070149e9f5c7a14b16bf9c604c46b1778d3dae4
Martinix75 <fintobersaglio@yahoo.it>
2021-09-30 22:05:49 +0200

    add for

--- scr/varie_3_7.nim
+++ scr/varie_3_7.nim
@@ -1,5 +1,16 @@
-var x = 1
-while x < 5:
-  echo("x ora vale: ", x)
-  inc x
-echo("uscito con x = ", x)
+for x in 1..10:
+  stdout.write(x, "-")
+echo()
+#let a = @[40,50,55,3]
+for x in @[40,50,55,3]:
+  stdout.write(x, "-")
+echo()
+for x in "ciao":
+  stdout.write(x, "-")
+echo()
+for x in countup(1, 10, 2):
+   stdout.write(x, "-")
+echo() 
+for x in countdown(20, 10, 2):
+   stdout.write(x, "-")
+echo()
