7edc746a650ecba39be2bd1e0cefda1c4a314669
Martinix75 <fintobersaglio@yahoo.it>
2021-10-13 23:23:55 +0200

    add tyr except

--- scr/varie_3_7.nim
+++ scr/varie_3_7.nim
@@ -1,16 +1,9 @@
-for x in 1..10:
-  stdout.write(x, "-")
-echo()
-#let a = @[40,50,55,3]
-for x in @[40,50,55,3]:
-  stdout.write(x, "-")
-echo()
-for x in "ciao":
-  stdout.write(x, "-")
-echo()
-for x in countup(1, 10, 2):
-   stdout.write(x, "-")
-echo() 
-for x in countdown(20, 10, 2):
-   stdout.write(x, "-")
-echo()
+proc provaEccezione(a, b :int)=
+  if a < b:
+    raise newException(IOError, "errore a<b")
+  else:
+    echo("tutto bene!")
+
+let a = 3
+let b = 7
+provaEccezione(a,b)
